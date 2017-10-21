3018881d18bcaa95bb11e61a8697aeabf7094281
Uncle Yong's Workshop <The_YongGrand@yahoo.com>
2017-10-21 09:39:04 +0800

    Added: Texas Instruments PCM5102 DAC

---
+++ v1_07_01/apps/sk1632-i2s-dma-pcm5102/firmware/sk1632-i2s-dma-pcm5102.X/build/default/production/_ext/1360937237/main.o.d
@@ -0,0 +1,5 @@
+build/default/production/_ext/1360937237/main.o:  \
+../src/main.c  \
+ ../../../../framework/system/common/sys_module.h  \
+ ../../../../framework/system/common/sys_common.h  \
+../src/main.h 
---
+++ v1_07_01/apps/sk1632-i2s-dma-pcm5102/firmware/src/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define BUFFER_LENGTH     512
+
+#define ENVELOPE_SPEED    128
+
+#define CARRIER_AMPL      3
+
+#define MOD_F_MULT     2
+#define MOD_F_DIV      1
+
+#define MOD_AMPL       2048      
+
+#define TEMPO_MULT    100  
+#define TEMPO_DIV     100
+
+
+
+
+
+#endif
